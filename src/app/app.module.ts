c99d94f1ca8265711a13007cace5f91bdf6acefc
Yadnesh Chaudhari <yadneshchaudhari2323@gmail.com>
2024-05-29 10:17:19 +0530

    All Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Mycomponents/nav/nav.component';
+import { FotterComponent } from './Mycomponents/fotter/fotter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FotterComponent
   ],
   imports: [
     BrowserModule,
